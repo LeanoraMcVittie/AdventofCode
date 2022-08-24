ada774a35663c95eb7de1bf6eabe831a1706236c
Leanora McVittie <leanora.mcvittie@gmail.com>
2022-08-23 22:31:58 -0700

    finished day 19 (also .gitignore update woo fun)

--- utils/utils.py
+++ utils/utils.py
@@ -1,27 +1,43 @@
 from typing import Any, Callable, Dict, List
 
+
 def get_input(module: str, prefix: str = "") -> List[str]:
-	prefix += "_" if prefix else ""
-	with open(f"{module}/{prefix}input.txt", 'r') as input_file:
-		values: List[str] = input_file.readlines()
-	return [v.strip() for v in values]
+    prefix += "_" if prefix else ""
+    with open(f"{module}/{prefix}input.txt", "r") as input_file:
+        values: List[str] = input_file.readlines()
+    return [v.strip() for v in values]
+
 
 def is_valid_hex(hex: str) -> bool:
-	try:
-		int(hex, base=16)
-	except ValueError:
-		return False
-	return True
+    try:
+        int(hex, base=16)
+    except ValueError:
+        return False
+    return True
+
 
 # Example usage: 2019 day 18
 def cache(cache_dict: Dict[Any, Any], compute_cache_key: Callable) -> Callable:
-	def wrap(func: Callable) -> Callable:
-		def wrapped_func(*args, **kwargs) -> Any:
-			cache_key = compute_cache_key(*args, **kwargs)
-			cached_result = cache_dict.get(cache_key)
-			if cached_result: return cached_result
-			result = func(*args, **kwargs)
-			cache_dict[cache_key] = result
-			return result
-		return wrapped_func
-	return wrap
+    def wrap(func: Callable) -> Callable:
+        def wrapped_func(*args, **kwargs) -> Any:
+            cache_key = compute_cache_key(*args, **kwargs)
+            cached_result = cache_dict.get(cache_key)
+            if cached_result:
+                return cached_result
+            result = func(*args, **kwargs)
+            cache_dict[cache_key] = result
+            return result
+
+        return wrapped_func
+
+    return wrap
+
+
+def binary_search(low: int, high: int, is_too_high: Callable[[int], bool]) -> int:
+    while high - low > 1:
+        midpoint = int((high - low) / 2) + low
+        if is_too_high(midpoint):
+            high = midpoint
+        else:
+            low = midpoint
+    return low
--- y19/19/part2.py
+++ y19/19/part2.py
@@ -1,35 +1,21 @@
 from typing import List
 from y19.intcode import IntCode
-from utils.field.two_d import Cell, Field
 
 
-def binary_search(low: int, high: int, is_too_high: Callable[[int], bool]) -> int:
-	while high - low > 1:
-		midpoint = int((high-low)/2)+low
-		if is_too_high(midpoint): high = midpoint
-		else: low = midpoint
-	return low
-
-
-def check_in_tractor_beam(program: str, x: int, y: int) -> bool:
-	tractor_beam = IntCode(program)
-	tractor_beam.add_input(x)
-	tractor_beam.add_input(y)
-	tractor_beam.run()
-	return tractor_beam.next_output() == 1
-
-
-def find_top_edge_of_tractor_beam_for_x_value(program: str, x: int) -> int:
+def is_in_tractor_beam(program: str, x: int, y: int) -> bool:
+    tractor_beam = IntCode(program)
+    tractor_beam.add_input(x)
+    tractor_beam.add_input(y)
+    tractor_beam.run()
+    return tractor_beam.next_output() == 1
 
 
 def run(input_data: List[str]) -> int:
-	# x = 0
-	# y = 0
-	# while True:
-	# 	x = int(input("x: "))
-	# 	y = int(input("y: "))
-	# 	if y == "stop" or x == "stop": break
-	# 	print(get_tractor_beam(input_data[0], x, y))
-	# return x * 10000 + y
-
-	return binary_search(0, 1000000, lambda x: x > 928)
+    program = input_data[0]
+    x = 0
+    y = 0
+    while not is_in_tractor_beam(program, x + 99, y):
+        y += 1
+        while not is_in_tractor_beam(program, x, y + 99):
+            x += 1
+    return x * 10000 + y
