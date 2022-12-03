ededf50c152f4ae11c49a1e2237f89f5369b3836
Leanora McVittie <leanora.mcvittie@gmail.com>
2022-12-02 21:13:53 -0800

    2022 Day 3! That was more like it

---
+++ y22/03/part1.py
@@ -0,0 +1,17 @@
+from typing import List
+
+def char_val(c: str) -> int:
+	if c.islower():
+		return ord(c) - 96
+	return ord(c) - 38
+
+def run(input_data: List[str]) -> int:
+	count = 0
+	for d in input_data:
+		one = d[:int(len(d)/2)]
+		two = d[int(len(d)/2):]
+		for c in one:
+			if c in two:
+				count += char_val(c)
+				break
+	return count
---
+++ y22/03/part2.py
@@ -0,0 +1,18 @@
+from typing import List
+
+def char_val(c: str) -> int:
+	if c.islower():
+		return ord(c) - 96
+	return ord(c) - 38
+
+def run(input_data: List[str]) -> int:
+	count = 0
+	for i in range(0, len(input_data), 3):
+		one = input_data[i]
+		two = input_data[i+1]
+		three = input_data[i+2]
+		for c in one:
+			if c in two and c in three:
+				count += char_val(c)
+				break
+	return count
