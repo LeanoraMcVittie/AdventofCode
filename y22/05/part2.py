ee36a01786438de091d9c3db647bea6f355a0947
Leanora McVittie <leanora.mcvittie@gmail.com>
2022-12-04 21:29:18 -0800

    2022 Day 5 -- almost 30 mins, almost all on part 1 bugs :/

---
+++ y22/05/part2.py
@@ -0,0 +1,28 @@
+from typing import List
+
+WHITESPACE = True
+NUM_COLS = 9
+
+def run(input_data: List[str]) -> int:
+	stacks = {}
+	j = 0
+	for datum in input_data:
+		j += 1
+		if datum == "\n":
+			break
+		for i in range(1, (NUM_COLS*4)+1, 4):
+			if (c := datum[i]) != " " and c.isupper():
+				stacks.setdefault((i//4)+1, []).append(c)
+	
+	for i in range(j, len(input_data)):
+		datum = input_data[i]
+		_, num, _, from_col, _, to_col = datum.split(" ")
+		num, from_col, to_col = [int(a) for a in [num, from_col, to_col]]
+		mov = stacks[from_col][:num]
+		stacks[from_col] = stacks[from_col][num:]
+		stacks[to_col] = mov + stacks[to_col]
+	
+	tops = ""
+	for i in range(1, NUM_COLS + 1):
+		tops += stacks[i][0]
+	return tops
